b9745bee4da39a4f87f648116660a4a692a94e1b
DavidsonWasserman <davidsomagalhaes@hotmail.com>
2025-06-15 21:01:07 -0300

    atualização banco de dados

--- VegaReviews.sql
+++ VegaReviews.sql
@@ -88,9 +88,10 @@ CREATE TABLE `usuarios` (
   `senha` varchar(100) NOT NULL,
   `sobre_mim` text,
   `foto_perfil` longtext,
+  `quantidade_de_reviews` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nickname` (`nickname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,6 +100,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Vega','senha','jogador de jogos','Vega_PFP.jpg',NULL);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -111,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-08  9:18:12
+-- Dump completed on 2025-06-15 20:58:30
